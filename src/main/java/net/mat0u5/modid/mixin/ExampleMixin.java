e12eb6afe8d931ed48167fcc5684224e8097d9e4
Mat0u5 <matous.danko+github@gmail.com>
2025-07-12 17:06:22 +0200

    Updates..

--- src/main/java/net/mat0u5/modid/mixin/ExampleMixin.java
+++ src/main/java/net/mat0u5/modid/mixin/ExampleMixin.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package net.mat0u5.modid.mixin;
 
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,6 +10,5 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "loadWorld")
 	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
 	}
 }
