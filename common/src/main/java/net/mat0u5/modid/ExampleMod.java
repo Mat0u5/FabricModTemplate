af20165b9a76c1ac4d173abac7a993b0d8a73c39
Mat0u5 <matous.danko@gmail.com>
2026-04-16 21:44:06 +0200

    javaa

--- common/src/main/java/net/mat0u5/modid/ExampleMod.java
+++ common/src/main/java/net/mat0u5/modid/ExampleMod.java
@@ -4,6 +4,6 @@ public final class ExampleMod {
     public static final String MOD_ID = "modid";
 
     public static void init() {
-
+        System.out.println("Example Mod Init!");
     }
 }
--- quilt/src/main/java/net/mat0u5/modid/quilt/ExampleModQuilt.java
+++ quilt/src/main/java/net/mat0u5/modid/quilt/ExampleModQuilt.java
@@ -1,13 +1,13 @@
 package net.mat0u5.modid.quilt;
 
-import org.quiltmc.loader.api.ModContainer;
-import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import net.fabricmc.api.ModInitializer;
 
 import net.mat0u5.modid.fabriclike.ExampleModFabricLike;
 
 public final class ExampleModQuilt implements ModInitializer {
+
     @Override
-    public void onInitialize(ModContainer mod) {
+    public void onInitialize() {
         // Run the Fabric-like setup.
         ExampleModFabricLike.init();
     }
