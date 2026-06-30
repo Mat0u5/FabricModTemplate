1d955979ef9f011e6f2f8dc0803fe38eed80c9fb
Mat0u5 <matous.danko@gmail.com>
2026-06-30 20:11:14 +0200

    Removed Platform

--- src/main/java/net/mat0u5/modid/Main.java
+++ src/main/java/net/mat0u5/modid/Main.java
@@ -1,17 +1,7 @@
 package net.mat0u5.modid;
 
-import net.mat0u5.modid.platform.Platform;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-//? fabric {
-import net.mat0u5.modid.platform.fabric.FabricPlatform;
-//?} neoforge {
-/*import net.mat0u5.modid.platform.neoforge.NeoforgePlatform;
- *///?} forge {
-/*import net.mat0u5.modid.platform.forge.ForgePlatform;
-*///?}
 
 public class Main {
 
@@ -20,29 +10,13 @@ public class Main {
 	public static final String MOD_FRIENDLY_NAME = "ModId Name";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	private static final Platform PLATFORM = createPlatformInstance();
-
 	public static void onInitialize() {
-		LOGGER.info("Initializing {} on {}", MOD_ID, platform().loader());
+		LOGGER.info("Initializing {}", MOD_ID);
 		LOGGER.info("{}: { version: {}; friendly_name: {} }", MOD_ID, MOD_VERSION, MOD_FRIENDLY_NAME);
 	}
 
 	public static void onInitializeClient() {
-		LOGGER.info("Initializing {} Client on {}", MOD_ID, platform().loader());
+		LOGGER.info("Initializing {} Client", MOD_ID);
 		LOGGER.info("{}: { version: {}; friendly_name: {} }", MOD_ID, MOD_VERSION, MOD_FRIENDLY_NAME);
-	}
-
-	static Platform platform() {
-		return PLATFORM;
-	}
-
-	private static Platform createPlatformInstance() {
-		//? fabric {
-		return new FabricPlatform();
-		//?} neoforge {
-		/*return new NeoforgePlatform();
-		 *///?} forge {
-		/*return new ForgePlatform();
-		*///?}
 	}
 }
