f67acc35c302bc7b94b4ccd32afb86618fc57cf4
Mat0u5 <matous.danko@gmail.com>
2026-07-01 00:48:30 +0200

    BUILD_UNBOUND_VERSION_RANGE setting, no jarJar in 26.1+

--- build.forge21.gradle.kts
+++ build.forge21.gradle.kts
@@ -69,9 +69,11 @@ dependencies {
 	annotationProcessor("org.spongepowered:mixin:${libs.versions.mixin.get()}:processor")
 	annotationProcessor("io.github.llamalad7:mixinextras-common:${libs.versions.mixinextras.get()}")
 
-	compileOnly("io.github.llamalad7:mixinextras-common:${libs.versions.mixinextras.get()}")
-	implementation("io.github.llamalad7:mixinextras-forge:${libs.versions.mixinextras.get()}")
-	"jarJar"("io.github.llamalad7:mixinextras-forge:${libs.versions.mixinextras.get()}")
+	if (stonecutter.eval(stonecutter.current.version, "< 26.1")) {
+		compileOnly("io.github.llamalad7:mixinextras-common:${libs.versions.mixinextras.get()}")
+		implementation("io.github.llamalad7:mixinextras-forge:${libs.versions.mixinextras.get()}")
+		"jarJar"("io.github.llamalad7:mixinextras-forge:${libs.versions.mixinextras.get()}")
+	}
 	implementation(libs.moulberry.mixinconstraints)
 }
 
