f86ca0a1f5b94bf2c5976ff14df48e3e766ae83f
Mat0u5 <matous.danko@gmail.com>
2026-07-07 17:17:24 +0200

    buildAndCollectNoDowngrader and Logger

--- src/main/java/net/mat0u5/modid/Main.java
+++ src/main/java/net/mat0u5/modid/Main.java
@@ -1,14 +1,8 @@
 package net.mat0u5.modid;
 
 import net.mat0u5.modid.platform.Platform;
-
-//? if <= 1.17 {
-/*import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-*///?} else {
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-//?}
 
 //? fabric {
 import net.mat0u5.modid.platform.fabric.FabricPlatform;
@@ -23,12 +17,7 @@ public class Main {
 	public static final String MOD_ID = "modid";
 	public static final String MOD_VERSION = "1.0.0";
 	public static final String MOD_FRIENDLY_NAME = "ModId Name";
-
-	//? if <= 1.17 {
-	/*public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-	 *///?} else {
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	//?}
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	private static final Platform PLATFORM = createPlatformInstance();
 
